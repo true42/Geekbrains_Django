f52294a52044fea33b5fe304be66f679565ebfe5
true42 <bezmaternykh.alexey@gmail.com>
2021-11-14 15:11:15 +0300

    create fixtures directory create products.json change сontroller products → add load data from json-file

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import json
 
 
 # Create your views here.
@@ -16,49 +17,13 @@ def index(request):
 
 
 def products(request):
-
+    with open('mainapp/fixtures/products.json', 'r', encoding='UTF-8') as json_data:
+        products_data = json.load(json_data)
     context = {
         'title': 'Geekshop - Каталог',
         'menu_items': [
             'Новинки', 'Одежда', 'Обувь', 'Аксессуары', 'Подарки'
         ],
-        'products': [
-            {
-                'name': 'Худи черного цвета с монограммами adidas Originals',
-                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-                'price': 6_090,
-                'photo': 'Adidas-hoodie.png',
-            },
-            {
-                'name': 'Синяя куртка The North Face',
-                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-                'price': 23_725,
-                'photo': 'Blue-jacket-The-North-Face.png',
-            },
-            {
-                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-                'price': 3_390,
-                'photo': 'Brown-sports-oversized-top-ASOS-DESIGN.png',
-            },
-            {
-                'name': 'Черный рюкзак Nike Heritage',
-                'description': 'Плотная ткань. Легкий материал.',
-                'price': 2_340,
-                'photo': 'Black-Nike-Heritage-backpack.png',
-            },
-            {
-                'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
-                'description': 'Гладкий кожаный верх. Натуральный материал.',
-                'price': 13_590,
-                'photo': 'Black-Dr-Martens-shoes.png',
-            },
-            {
-                'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
-                'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
-                'price': 2_890,
-                'photo': 'Dark-blue-wide-leg-ASOs-DESIGN-trousers.png',
-            },
-        ]
+        'products': products_data['products']
     }
     return render(request, 'mainapp/products.html', context)
