36fc25a7c9a308d6353b8fe8c06e1aa284f08eff
true42 <bezmaternykh.alexey@gmail.com>
2021-11-17 20:59:11 +0300

    add model Product_Category

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product_Category(models.Model):
+    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
