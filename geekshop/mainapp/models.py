bb224b090fea578f9817c0c8a48c15dc0f95281d
true42 <bezmaternykh.alexey@gmail.com>
2021-11-18 18:06:38 +0300

    Sorry, I did not record the changes step by step. Next time I will be more careful. Homework_3 done!

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -18,4 +18,4 @@ class Product(models.Model):
     category = models.ForeignKey(Product_Category, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return f'{self.name} | {self.category}'
