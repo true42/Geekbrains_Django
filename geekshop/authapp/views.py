32c7e4171cb44d2dc1ed86c4b9aed32c004eec0c
true42 <bezmaternykh.alexey@gmail.com>
2021-11-21 13:34:54 +0300

    create forms login and register

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,15 +1,44 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from authapp.forms import UserLoginForm, UserRegisterForm
 
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = auth.authenticate(username=username, password=password)
+            if user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
     context = {
-        'title': 'Geekshop | Авторизация'
+        'title': 'Geekshop | Авторизация',
+        'form': form,
     }
     return render(request, 'authapp/login.html', context)
 
 
 def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('authapp:login'))
+        else:
+            print(form.errors)
+    else:
+        form = UserRegisterForm()
     context = {
-        'title': 'Geekshop | Регистрация'
+        'title': 'Geekshop | Регистрация',
+        'form': form,
     }
     return render(request, 'authapp/register.html', context)
