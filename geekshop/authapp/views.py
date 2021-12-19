028b0d5dcd580edcccc6006d4d54a79eb950749f
true42 <bezmaternykh.alexey@gmail.com>
2021-12-19 16:01:26 +0300

    add email account activation

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,9 +1,11 @@
+from django.conf import settings
 from django.contrib.auth.views import LoginView, LogoutView
+from django.core.mail import send_mail
 from django.views.generic import FormView, UpdateView
 
 from authapp.models import User
 from baskets.models import Baskets
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy, reverse
@@ -29,14 +31,41 @@ class RegisterListView(FormView, BaseClassContextMixin):
 
         form = self.form_class(data=request.POST)
         if form.is_valid():
-            form.save()
-            messages.set_level(request, messages.SUCCESS)
-            messages.success(request, 'Вы успешно зарегистрировались!')
-            return HttpResponseRedirect(reverse('authapp:login'))
+            user = form.save()
+            if self.send_verify_link(user):
+                messages.set_level(request, messages.SUCCESS)
+                messages.success(request, 'Вы успешно зарегистрировались!')
+                return HttpResponseRedirect(reverse('authapp:login'))
+            else:
+                messages.set_level(request, messages.ERROR)
+                messages.error(request, form.errors)
         else:
             messages.set_level(request, messages.ERROR)
             messages.error(request, form.errors)
         return render(request, self.template_name, {'form': form})
+
+    def send_verify_link(self, user):
+        verify_link = reverse('authapp:verify', args=[user.email, user.activation_key])
+        subject = f'Для активации учётной записи {user.username} пройдите по ссылке'
+        messages = f'Для подтверждения учётной записи {user.username} на портале \n {settings.DOMAIN_NAME}{verify_link}'
+        return send_mail(subject, messages, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+
+    def verify(self, email, activate_key):
+        try:
+            user = User.objects.get(email=email)
+            if user and user.activation_key == activate_key and not user.is_activation_key_expires():
+                user.activation_key = ''
+                user.activation_key_expires = None
+                user.is_active = True
+                user.save()
+                auth.login(self, user)
+            return render(self, 'authapp/verification.html')
+
+        except Exception as e:
+            return HttpResponseRedirect(reverse('index'))
+
+
+
 
 
 class ProfileFormView(UpdateView, BaseClassContextMixin, UserDispatchMixin):
--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-nwrwig7ut+$s1@--r1+-oe!_n^ypuwa9+lzh50$!e-^_&ekqju'
+from dotenv import load_dotenv
+load_dotenv(BASE_DIR / '.env')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -137,3 +139,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authapp.User'
 LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = '/'
+
+DOMAIN_NAME = 'http://localhost:8000'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') == 'True' else False
+
+# EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# EMAIL_FILE_PATH = 'tmp/emails/'
+
+EMAIL_HOST_USER ,EMAIL_HOST_PASSWORD = None,None
+# py -m smtpd -n -c DebuggingServer localhost:25
