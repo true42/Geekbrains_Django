ec67a54e66710ce5e9681a63ac5bec61cb78a13f
true42 <bezmaternykh.alexey@gmail.com>
2021-12-24 17:43:16 +0300

    homework 2*

--- geekshop/authapp/pipelines.py
+++ geekshop/authapp/pipelines.py
@@ -13,7 +13,7 @@ def save_user_profile(backend, user, response, *args, **kwargs):
         return
 
     api_url = urlunparse(('http', 'api.vk.com', 'method/users.get', None,
-                          urlencode(OrderedDict(fields=','.join(('bdate', 'sex', 'about', 'photo_200')),
+                          urlencode(OrderedDict(fields=','.join(('bdate', 'sex', 'about', 'photo_400_orig')),
                                                 access_token=response['access_token'], v=5.131)), None)
                          )
 
@@ -42,5 +42,15 @@ def save_user_profile(backend, user, response, *args, **kwargs):
     if age < 18:
         user.delete()
         raise AuthForbidden('social_core.backends.vk.VKOAuth2')
+
+    if data['photo_400_orig']:
+        photo_link = data['photo_400_orig']
+        photo_response = requests.get(photo_link)
+        path_photo = f'users_image/{user.pk}.jpg'
+        with open(f'media/{path_photo}', "wb") as photo:
+            photo.write(photo_response.content)
+
+        user.image = path_photo
+
 
     user.save()
