54989194fa2fff196ec800f3edf897aa6dc0b6bc
true42 <bezmaternykh.alexey@gmail.com>
2021-12-29 20:01:21 +0300

    save/delete baskets/ordersapp

--- geekshop/baskets/views.py
+++ geekshop/baskets/views.py
@@ -5,22 +5,6 @@ from django.template.loader import render_to_string
 from baskets.models import Baskets
 from mainapp.models import Product
 
-
-# @login_required
-# def basket_add(request, id):
-#     user_select = request.user
-#     product = Product.objects.get(id=id)
-#     baskets = Baskets.objects.filter(user=user_select, product=product)
-#
-#     if baskets:
-#         basket = baskets.first()
-#         basket.quantity += 1
-#         basket.save()
-#     else:
-#         Baskets.objects.create(user=user_select, product=product, quantity=1)
-#
-#     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-
 @login_required
 def basket_add(request, id):
     user_select = request.user
