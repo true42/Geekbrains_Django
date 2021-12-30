45bda777106338dc9e0b04a6bc2955291e9f5e5c
true42 <bezmaternykh.alexey@gmail.com>
2021-12-30 18:03:57 +0300

    postgres and requirements.txt

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -88,10 +88,18 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Geekshop',
+        'USER': 'postgres',
     }
 }
 
